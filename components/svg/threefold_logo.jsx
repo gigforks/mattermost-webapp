94ba83f3b63fd6fe48a16665e19762ddc477195d
adelsamar <adels@greenitglobe.com>
2018-05-20 13:24:12 +0200

    Fix svg file

--- components/svg/threefold_logo.jsx
+++ components/svg/threefold_logo.jsx
@@ -7,25 +7,25 @@ export default class ThreeFoldLogo extends React.PureComponent {
     render() {
         return (
             <span {...this.props}>
-                <svg
-                    version='1.1'
-                    id='Layer_1'
-                    x='0px'
-                    y='0px'
-                    viewBox='0 0 566.9 566.9'
+                <svg 
+                    version="1.1" 
+                    id="Layer_1" 
+                    x="0px" 
+                    y="0px"
+                    viewBox="0 0 566.9 566.9" 
                     style={style.background}
                 >
                     <g>
                         <g>
-                            <circle class="st0" cx="284.5" cy="283.5" r="157.2" />
+                            <circle style={style.st0} cx="284.5" cy="283.5" r="157.2" />
                         </g>
                         <g>
-                            <circle class="st1" cx="282.5" cy="277.5" r="157.2" />
+                            <circle style={style.st1} cx="282.5" cy="277.5" r="157.2" />
                         </g>
                         <g>
-                            <rect x="193.1" y="214.1" class="st2" width="178.8" height="12.9" />
-                            <rect x="193.1" y="328" class="st0" width="178.8" height="12.9" />
-                            <rect x="193.1" y="271" class="st3" width="178.8" height="12.9" />
+                            <rect x="193.1" y="214.1" style={style.st2} width="178.8" height="12.9" />
+                            <rect x="193.1" y="328" style={style.st0} width="178.8" height="12.9" />
+                            <rect x="193.1" y="271" style={style.st3} width="178.8" height="12.9" />
                         </g>
                     </g>
                 </svg>
@@ -38,8 +38,16 @@ const style = {
     background: {
         enableBackground: 'new 0 0 566.9 566.9',
     },
-    st0: { fill: '#C6C9CD' },
-    st1: { fill: '#FFFFFF' },
-    st2: { fill: '#698FC9' },
-    st3: { fill: '#7DA783' },
+    st0: {
+        fill: 'C6C9CD'
+    },
+    st1: { 
+        fill: 'FFFFFF'
+    },
+    st2: {
+        fill: '698FC9'
+    },
+    st3: { 
+        fill: '7DA783'
+    },
 };
